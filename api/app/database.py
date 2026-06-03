15b89b8b5c8b37afb9e084cf428ff69070329e2e
Zhenzhen Chen <czz0917@gmail.com>
2026-06-03 15:58:51 -0400

    Dockerize api and web services

--- api/app/database.py
+++ api/app/database.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-DATABASE_URL = "sqlite:///./dev.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")
 
 engine = create_engine(
     DATABASE_URL,
